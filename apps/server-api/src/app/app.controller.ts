8204a663925a82fda51c9653ec81932b8aad927b
execode-bf <bartlomiej.filipiak@execode.pl>
2023-08-17 15:35:57 +0200

    Add swagger

--- apps/server-api/src/app/app.controller.ts
+++ apps/server-api/src/app/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-
+import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags("App")
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('getData')
   getData() {
     return this.appService.getData();
   }
--- apps/server-api/src/main.ts
+++ apps/server-api/src/main.ts
@@ -5,15 +5,29 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
+  // App configuration
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API Description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Set port and start app
   const port = process.env.PORT || 3000;
   await app.listen(port);
+
+  // Info log
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
