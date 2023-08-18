700ac84a7e99991d20d68a603221e874f5fe7212
execode-bf <bartlomiej.filipiak@execode.pl>
2023-08-19 01:23:56 +0200

    Add client mobile

---
+++ apps/client/client-mobile/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'client-mobile',
+  webDir: '../../../dist/apps/client-mobile',
+  bundledWebRuntime: false,
+};
+
+export default config;
