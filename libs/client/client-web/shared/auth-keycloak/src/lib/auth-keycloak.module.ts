884b439ab268fc6ba22e96e671d7f52520eee538
execode-bf <bartlomiej.filipiak@execode.pl>
2023-08-28 21:56:33 +0200

    Add keycloak auth to angular

---
+++ libs/client/client-web/shared/auth-keycloak/src/lib/auth-keycloak.module.ts
@@ -0,0 +1,34 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:28080',
+        realm: 'execode',
+        clientId: 'nest-app'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
+
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule, KeycloakAngularModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
+  bootstrap: []
+})
+export class AuthKeycloakModule {}
