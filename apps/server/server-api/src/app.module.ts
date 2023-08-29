018b8156f027938249fb593dc44810fe852ca684
execode-bf <bartlomiej.filipiak@execode.pl>
2023-08-29 20:26:28 +0200

    Add pino logger

--- apps/server/server-api/src/app.module.ts
+++ apps/server/server-api/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { FeatureUserModule } from '@exe/server/server-api/feature/feature-user';
+import { LoggerPinoModule } from '@exe/server/shared/logger-pino';
 
 @Module({
-  imports: [FeatureUserModule]
+  imports: [LoggerPinoModule, FeatureUserModule]
 })
 export class AppModule {}
