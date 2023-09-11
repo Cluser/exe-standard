1c957ec6c7f2b345f8c38645254771c4bf2e6cc3
execode-bf <bartlomiej.filipiak@execode.pl>
2023-09-12 00:57:36 +0200

    Add primeng

--- apps/client/client-web/src/app/app.module.ts
+++ apps/client/client-web/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthKeycloakModule } from '@exe/client/client-web/shared/auth-keycloak';
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     AuthKeycloakModule
