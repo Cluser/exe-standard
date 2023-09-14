ef154c3060c8c0c2c87886e55aa38cfd5903e75e
execode-bf <bartlomiej.filipiak@execode.pl>
2023-09-14 10:33:37 +0200

    Rework mobile client

--- apps/client/client-mobile/capacitor.config.ts
+++ apps/client/client-mobile/capacitor.config.ts
@@ -2,9 +2,12 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'client-mobile',
-  webDir: '../../../dist/apps/client-mobile',
+  appName: 'client/client-mobile',
+  webDir: '../../../dist/apps/client/client-mobile',
   bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https',
+  },
 };
 
 export default config;
--- apps/client/client-mobile/src/app/app.component.ts
+++ apps/client/client-mobile/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '-root',
+  selector: 'exe-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
