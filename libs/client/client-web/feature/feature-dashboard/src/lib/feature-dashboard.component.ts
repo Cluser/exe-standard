d0a4711646de053aacec54bdc35142ad6e530ec6
execode-bf <bartlomiej.filipiak@execode.pl>
2023-09-20 00:00:00 +0200

    Add facade structure

--- libs/client/client-web/feature/feature-dashboard/src/lib/feature-dashboard.component.ts
+++ libs/client/client-web/feature/feature-dashboard/src/lib/feature-dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AppService } from '@exe/client/shared/data-access';
-import { KeycloakService } from 'keycloak-angular'
+import { FeatureDashboardFacadeService } from './feature-dashboard.facade';
 
 @Component({
   selector: 'exe-feature-dashboard',
@@ -9,23 +8,13 @@ import { KeycloakService } from 'keycloak-angular'
 })
 export class FeatureDashboardComponent implements OnInit {
 
-  constructor(private api: AppService, private keycloakService: KeycloakService) {}
+  constructor(private featureDashboardFacadeService: FeatureDashboardFacadeService) {}
 
   ngOnInit(): void {
-    this.api.userControllerGetUsers().subscribe((users) => {
-      console.log(users);
-      this.keycloakService.getToken().then((token) => {
-        console.log(token);
-      });
-
-      console.log(this.keycloakService.getUserRoles());
-      console.log(this.keycloakService.getUsername());
-    });   
+    this.featureDashboardFacadeService.getToken();
   }
 
   onLogoutClick(): void {
-    this.keycloakService.logout();
+    this.featureDashboardFacadeService.logout();
   }
-
-  
 }
---
+++ libs/client/client-web/feature/feature-dashboard/src/lib/feature-dashboard.facade.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { AppService } from '@exe/client/shared/data-access';
+import { KeycloakService } from 'keycloak-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeatureDashboardFacadeService {
+
+    constructor(
+        private api: AppService, 
+        private keycloakService: KeycloakService
+    ) {}
+
+    getToken(): void {
+        this.api.userControllerGetUsers().subscribe((users) => {
+            console.log(users);
+            this.keycloakService.getToken().then((token) => {
+                console.log(token);
+            });
+        
+            console.log(this.keycloakService.getUserRoles());
+            console.log(this.keycloakService.getUsername());
+        });   
+    }
+
+    logout(): void {
+        this.keycloakService.logout();
+    }
+}
