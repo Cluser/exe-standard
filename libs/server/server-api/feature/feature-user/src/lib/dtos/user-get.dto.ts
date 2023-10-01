5c62cf7759a8211bdd254bf612276e3a244861cb
execode-bf <bartlomiej.filipiak@execode.pl>
2023-10-01 18:57:25 +0200

    [ES-3] Dodanie obsługi prettier

--- libs/server/server-api/feature/feature-user/src/lib/dtos/user-get.dto.ts
+++ libs/server/server-api/feature/feature-user/src/lib/dtos/user-get.dto.ts
@@ -1,26 +1,26 @@
-import { User } from ".prisma/client";
-import { ApiPropertyOptional } from "@nestjs/swagger";
-import { IsNumber, IsOptional, IsString } from 'class-validator'
+import { User } from '.prisma/client';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class UserGetDto implements User {
-    @ApiPropertyOptional()
-    @IsNumber()
-    @IsOptional()
-    id: number;
+  @ApiPropertyOptional()
+  @IsNumber()
+  @IsOptional()
+  id: number;
 
-    @ApiPropertyOptional()
-    @IsString()
-    @IsOptional()
-    name: string;
+  @ApiPropertyOptional()
+  @IsString()
+  @IsOptional()
+  name: string;
 
-    @ApiPropertyOptional()
-    @IsString()
-    @IsOptional()
-    surname: string;
-  
-    constructor(id: number, name: string, surname: string) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-    }
+  @ApiPropertyOptional()
+  @IsString()
+  @IsOptional()
+  surname: string;
+
+  constructor(id: number, name: string, surname: string) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+  }
 }
