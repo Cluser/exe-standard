00b76eeab8c59af3f03c8afea8e6f45938b31a18
execode-bf <bartlomiej.filipiak@execode.pl>
2023-10-02 23:42:02 +0200

    [ES-5] Create data access nx library

--- libs/client/client-web/shared/store/src/lib/users/users.effects.ts
+++ libs/client/client-web/shared/store/src/lib/users/users.effects.ts
@@ -5,21 +5,16 @@ import { of } from 'rxjs';
 import { AppService } from '@exe/client/shared/data-access';
 import * as fromUsers from './index';
 
-
-
 @Injectable()
 export class UsersEffects {
-    constructor(
-        private readonly actions$: Actions,
-        private api: AppService
-    ) {}
+  constructor(private readonly actions$: Actions, private api: AppService) {}
 
-    getUsers$ = createEffect(() =>
-        this.actions$.pipe(
-            ofType(fromUsers.fetchUsers.type),
-            switchMap(() => this.api.userControllerGetUsers(undefined, '',  '')),
-            map(users => fromUsers.fetchUsersSuccess({ payload: users.data })),
-            catchError((error) => of(fromUsers.fetchUsersFailure({ payload: error })))
-        )
-    );
+  getUsers$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(fromUsers.fetchUsers.type),
+      switchMap(() => this.api.userControllerGetUsers()),
+      map((users) => fromUsers.fetchUsersSuccess({ payload: users.data })),
+      catchError((error) => of(fromUsers.fetchUsersFailure({ payload: error })))
+    )
+  );
 }
