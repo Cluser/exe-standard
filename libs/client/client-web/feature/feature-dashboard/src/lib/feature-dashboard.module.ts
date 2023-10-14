ec243f9e737807e5f8618ec691b26fd1042aa94d
execode-bf <bartlomiej.filipiak@execode.pl>
2023-10-14 20:29:25 +0200

    [ES-11] Add spectator

--- libs/client/client-web/feature/feature-dashboard/src/lib/feature-dashboard.module.ts
+++ libs/client/client-web/feature/feature-dashboard/src/lib/feature-dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FeatureDashboardComponent } from './feature-dashboard.component';
 import { FeatureDashboardRoutingModule } from './feature-dashboard-routing.module';
 import { LoaderModule } from '@exe/client/client-web/shared/loader'
+
 @NgModule({
   imports: [
     CommonModule,
