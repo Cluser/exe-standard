9d328d47ea9d9e6f989ca3b705f901604556a5d8
execode-bf <bartlomiej.filipiak@execode.pl>
2023-10-26 01:28:49 +0200

    [ES-22] Create socket.io server

--- libs/server/server-api/core/core/src/lib/core.module.ts
+++ libs/server/server-api/core/core/src/lib/core.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthKeycloakModule } from '@exe/server/server-api/core/auth-keycloak';
 import { LoggerPinoModule } from '@exe/server/server-api/core/logger-pino';
+import { SocketIoModule } from '@exe/server/server-api/shared/socket-io';
 
 @Module({
   imports: [
     AuthKeycloakModule,
-    LoggerPinoModule
+    LoggerPinoModule,
+    SocketIoModule
   ],
   exports: [
     AuthKeycloakModule,
-    LoggerPinoModule
+    LoggerPinoModule,
+    SocketIoModule
   ]
 })
 export class CoreModule {}
---
+++ libs/server/server-api/shared/socket-io/README.md
@@ -0,0 +1,7 @@
+# server-server-api-shared-socket-io
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test server-server-api-shared-socket-io` to execute the unit tests via [Jest](https://jestjs.io).
