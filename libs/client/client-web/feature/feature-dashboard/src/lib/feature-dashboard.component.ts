beca5f40aa42af48242cf3b8efae5a26fe2de2a1
execode-bf <bartlomiej.filipiak@execode.pl>
2023-10-28 00:04:20 +0200

    [ES-22] Socket io changes and dashboard clear

--- libs/client/client-web/feature/feature-dashboard/src/lib/feature-dashboard.component.ts
+++ libs/client/client-web/feature/feature-dashboard/src/lib/feature-dashboard.component.ts
@@ -1,7 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FeatureDashboardFacadeService } from './feature-dashboard.facade';
-import { UserGetResposeDto } from '@exe/client/shared/data-access';
-import { Observable } from 'rxjs';
 import { MENU_ITEMS, MenuItem } from './menu/menu.model';
 import { TOP_BAR_ITEMS } from './top-bar/top-bar.model';
 import { MenuItem as Menu} from 'primeng/api';
@@ -18,23 +16,11 @@ export class FeatureDashboardComponent implements OnInit {
     private featureDashboardFacadeService: FeatureDashboardFacadeService
   ) {}
   
-  get users$(): Observable<UserGetResposeDto[]> {
-    return this.featureDashboardFacadeService.getUsers$();
-  }
-
-  get isUsersLoading$(): Observable<boolean> {
-    return this.featureDashboardFacadeService.isUsersLoading$();
-  }
-
   get breadcrumbs(): Menu[] {
     return this.featureDashboardFacadeService.getBreadcrumbs();
   }
 
-  ngOnInit(): void {
-    this.featureDashboardFacadeService.getToken();
-    this.featureDashboardFacadeService.setLocalStorageData();
-    this.featureDashboardFacadeService.getLocalStorageData();
-  }
+  ngOnInit(): void {}
 
   onMenuItemClick(menuItem: MenuItem): void {
     switch(menuItem) {
--- libs/server/server-api/feature/feature-user/src/lib/user.module.ts
+++ libs/server/server-api/feature/feature-user/src/lib/user.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { PrismaClientModule } from '@exe/server/shared/prisma-client';
 import { UserService } from './user.service';
-// import { AuthKeycloakModule } from '@exe/server/shared/auth-keycloak'
 
 @Module({
   imports: [PrismaClientModule],
